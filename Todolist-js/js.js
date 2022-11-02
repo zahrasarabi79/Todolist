89caa77c3d09296bae6c0550f2018842a70562f9
zahra sarabi <zahrasarabi1379@gmail.com>
2022-11-02 15:49:21 +0330

    add new v

--- Todolist-js/js.js
+++ Todolist-js/js.js
@@ -2,11 +2,19 @@ const todoInput = document.querySelector(".works");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-container");
 const filterOption = document.querySelector(".filter-todos");
+
 todoButton.addEventListener("click", addToDo);
 todoList.addEventListener("click", checkremove);
 filterOption.addEventListener("click", filterTodos);
+document.addEventListener("DOMContentLoaded", getLocalTodos);
+// وقتی که اولین بار  صفحه داکیومت دام رو باز کردی
+// این فانکشن گت لوکال تو دوز را صدا بزن
+
 function addToDo(_e) {
   _e.preventDefault();
+  // از اونجایی که دکمه سابمیت رو زدیم.
+  // هر بار که بزنیم اطلاعات ذخیره می کنه و صفحه رفرش میشه یعنی اطلاعات از روز صفحه پاک میشه
+  //  ولی ما این رو نمی خوایم برا همین در فانکشن دکمه  این کد وارد می کنیم .
   console.log(_e);
   // get to do value
   // creat nea to do
@@ -24,6 +32,7 @@ function addToDo(_e) {
   todoDiv.innerHTML = newTodo;
   //   append to dom
   todoList.appendChild(todoDiv);
+  saveLocalTodos(todoInput.value);
   todoInput.value = "";
 }
 function checkremove(e) {
@@ -35,6 +44,7 @@ function checkremove(e) {
     todo.classList.toggle("completed");
   } else if (classList[1] === "fa-trash") {
     const todo = item.parentElement.parentElement;
+    removeLocalTodS(todo);
     todo.remove();
   }
 }
@@ -65,4 +75,37 @@ function filterTodos(e) {
         break;
     }
   });
+}
+function saveLocalTodos(todo) {
+  let savedTodos = localStorage.getItem("todos")
+    ? JSON.parse(localStorage.getItem("todos"))
+    : [];
+  savedTodos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(savedTodos));
+}
+
+function getLocalTodos(todo) {
+  let savedTodos = localStorage.getItem("todos")
+    ? JSON.parse(localStorage.getItem("todos"))
+    : [];
+  savedTodos.forEach((todo) => {
+    const todoDiv = document.createElement("div");
+
+    todoDiv.classList.add("todo");
+    const newTodo = `
+    <li>${todoInput.value}</li>
+    <span><i class="fa-solid fa-square-check"></i></span>
+    <span><i class="fa-solid fa-trash"></i></span>
+    <span><i class="fa-solid fa-pen-to-square"></i></span>
+  `;
+    todoDiv.innerHTML = newTodo;
+  });
+}
+function removeLocalTodS(todos) {
+  // باید اسم رو ببینیم با اسم داخل کادر مقایسه کنیم اگر درست بود خود متن به همراه کادر (پدربزرگ)
+  // را حذف کنیم .
+  console.log(todos.children[0]);
+  let savedTodos = localStorage.getItem("todos")
+    ? JSON.parse(localStorage.getItem("todos"))
+    : [];
 }
